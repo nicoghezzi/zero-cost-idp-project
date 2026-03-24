d61b45f66c84760e74cac1406aac4d3f3510b605
Nico Ghezzi <nicolas.dario.ghezzi@gmail.com>
2026-03-24 15:40:37 -0400

    Final working infra and UI with public access policy

--- infra/oidc.tf
+++ infra/oidc.tf
@@ -1,32 +1,52 @@
-/* # Comment this out so GitHub Actions doesn't try to recreate it
-resource "aws_iam_openid_connect_provider" "github" {
-  url             = "https://token.actions.githubusercontent.com"
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
-}
-*/
-
+# 1. Keep the Role
 resource "aws_iam_role" "github_role" {
   name = "github-action-role"
-
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [
-      {
+    Statement = [{
         Action = "sts:AssumeRoleWithWebIdentity",
         Effect = "Allow",
-        Principal = {
-          # Use the hardcoded ARN from your terminal output
-          Federated = "arn:aws:iam::943066268094:oidc-provider/token.actions.githubusercontent.com"
-        },
-        Condition = {
-          StringLike = {
-            "token.actions.githubusercontent.com:sub": "repo:nicoghezzi/zero-cost-idp-project:*"
-          }
-        }
-      }
-    ]
+        Principal = { Federated = "arn:aws:iam::943066268094:oidc-provider/token.actions.githubusercontent.com" },
+        Condition = { StringLike = { "token.actions.githubusercontent.com:sub": "repo:nicoghezzi/zero-cost-idp-project:*" } }
+    }]
   })
 }
 
-# ... (Keep the rest of your S3 and output code below)
+# 2. ADD THIS BACK - This is why it was trying to delete the policy!
+resource "aws_iam_role_policy_attachment" "admin_policy" {
+  role       = aws_iam_role.github_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+# 3. S3 Infrastructure
+resource "aws_s3_bucket" "website" {
+  bucket = "my-idp-site-0652f8f0"
+}
+
+resource "aws_s3_bucket_website_configuration" "site_config" {
+  bucket = aws_s3_bucket.website.id
+  index_document { suffix = "index.html" }
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.website.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+# 4. The Policy that fixes the 403 error
+resource "aws_s3_bucket_policy" "allow_public_read" {
+  bucket = aws_s3_bucket.website.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.website.arn}/*"
+    }]
+  })
+}
