dd7d243e4e62b3251ab733c74d948e5af04a0271
Nico Ghezzi <nicolas.dario.ghezzi@gmail.com>
2026-03-24 15:05:57 -0400

    Finalize infra and add index.html

--- infra/oidc.tf
+++ infra/oidc.tf
@@ -1,11 +1,11 @@
-# This creates the "Trust" between AWS and GitHub
+/* # Comment this out so GitHub Actions doesn't try to recreate it
 resource "aws_iam_openid_connect_provider" "github" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
 }
+*/
 
-# This creates the "Role" that GitHub will "wear" when it logs into AWS
 resource "aws_iam_role" "github_role" {
   name = "github-action-role"
 
@@ -16,7 +16,8 @@ resource "aws_iam_role" "github_role" {
         Action = "sts:AssumeRoleWithWebIdentity",
         Effect = "Allow",
         Principal = {
-          Federated = aws_iam_openid_connect_provider.github.arn
+          # Use the hardcoded ARN from your terminal output
+          Federated = "arn:aws:iam::943066268094:oidc-provider/token.actions.githubusercontent.com"
         },
         Condition = {
           StringLike = {
@@ -28,47 +29,4 @@ resource "aws_iam_role" "github_role" {
   })
 }
 
-# This gives the Role permission to build things (Admin access)
-resource "aws_iam_role_policy_attachment" "admin_policy" {
-  role       = aws_iam_role.github_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
-}
-
-# This prints out the ID of the role so we can use it in GitHub later
-output "github_role_arn" {
-  value = aws_iam_role.github_role.arn
-}
-
-# This creates a unique name for your bucket
-resource "random_id" "bucket_suffix" {
-  byte_length = 4
-}
-
-# This creates the actual S3 Bucket (The "Folder" in the Cloud)
-resource "aws_s3_bucket" "website" {
-  bucket = "my-idp-site-${random_id.bucket_suffix.hex}"
-}
-
-# This tells AWS the bucket is for a Website
-resource "aws_s3_bucket_website_configuration" "site_config" {
-  bucket = aws_s3_bucket.website.id
-
-  index_document {
-    suffix = "index.html"
-  }
-}
-
-# This makes the bucket public so people can see your site
-resource "aws_s3_bucket_public_access_block" "public_access" {
-  bucket = aws_s3_bucket.website.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
-
-# This prints the URL of your new website
-output "website_url" {
-  value = aws_s3_bucket_website_configuration.site_config.website_endpoint
-}
+# ... (Keep the rest of your S3 and output code below)
